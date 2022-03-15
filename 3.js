0c1fe806445546fed25e4298bde73156c387f51d
eunhyung <ooooo5h91@gmail.com>
2022-03-15 22:50:26 +0900

    in array : not for ~ in, use for ~ of

--- 3.js
+++ 3.js
@@ -1,8 +1,9 @@
 let days = ['mon', 'tue', 'wed']
 
-for (let index = 0; index < days.length ; index++) {
-  console.log(days[index])
+for (let day of days) {
+  console.log(day)
   
 }
 
 // 자바스크립트에서의 배열은 처음 시작조건, 종료될 조건, 증감 이런 식으로 코드를 작성한다
+// 자바스크립트에서 배열의 for반복문은 in보다 of를 쓰는 게 더 좋다고 함
