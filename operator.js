457cfc3e7dfaaab8cd74a06d20c3a03dc2b80086
eunhyung <ooooo5h91@gmail.com>
2022-03-16 20:45:01 +0900

    comparison operators

--- operator.js
+++ operator.js
@@ -33,3 +33,9 @@ x += y ; // x = x + y;
 x -= y ;
 x *= y;
 x /= y;
+
+// 5. Comparison operators
+console.log(10 < 6); //less than
+console.log(10 <= 6);  // less than or equal
+console.log(10 > 6); // greater than
+console.log(10 >= 6); // greater than or equal
