1cfc5347b5ef165cbd9d30ef02dd265e9512d117
eunhyung <ooooo5h91@gmail.com>
2022-03-16 21:59:57 +0900

    loops
    - while, do while, for

--- operator.js
+++ operator.js
@@ -124,3 +124,53 @@ switch (browser) {
         console.log('same all!');
         break;
 }
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while ( i > 0) {
+    console.log(`while : ${i}`);
+    i--;
+} 
+
+// do while loop, body code is executed first,
+// then check the condition.
+do {
+    console.log(`do while : ${i}`);
+    i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+    console.log(`for : ${i}`);
+}
+
+for (let i = 3; i > 0; i = i -2) {
+    // inline variable declaration
+    console.log(`inline variable for : ${i}`);
+}
+
+// nested loops
+for ( let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log(`i: ${i}, j:${j}`);
+    }
+}
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i <= 10; i ++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(`2의 배수 : ${i}`)
+}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (i = 0; i <= 10 ; i++) {
+    if (i > 8) {
+        break
+    }
+    console.log(`${i}`)
+}
