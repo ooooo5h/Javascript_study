76233f3f8515c97161162f8b77d337fd9c9b3d82
eunhyung <ooooo5h91@gmail.com>
2022-03-16 21:22:05 +0900

    switch case
    - 많은 If조건들이 나열할 때는 switch문을 쓰는게 가독성이 좋다

--- operator.js
+++ operator.js
@@ -106,3 +106,21 @@ if(name === 'eunhyung') {
 // 9. Ternary operator : ?
 // condition ? value1 : value2;
 console.log(name === 'eunhyung' ? 'yes' : 'no');
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multuple type checks in TS
+const browser = 'IE';
+switch (browser) {
+    case 'IE' :
+        console.log('go away!');
+        break;
+    case 'Chrome' :
+    case 'Firefox' :
+        console.log('love you!');
+        break;
+    default :
+        console.log('same all!');
+        break;
+}
