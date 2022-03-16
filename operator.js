5c6893f5c04ac799c4d5d35e1f324c00ca6b5056
eunhyung <ooooo5h91@gmail.com>
2022-03-16 20:42:18 +0900

    increment & decrement operators

--- operator.js
+++ operator.js
@@ -10,3 +10,18 @@ console.log(1 / 1);  // divide
 console.log(1 * 1);  // multiply
 console.log(5 % 2);  // remainder
 console.log(2 ** 3);  // exponentiation  
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement : ${preIncrement}, counter : ${counter}`)
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement : ${postIncrement}, counter : ${counter}`)
+const preDecrement = --counter;
+console.log(`preDecrement : ${preDecrement}, counter : ${counter}`)
+const postDecrement = counter--;
+console.log(`postDecrement : ${postDecrement}, counter : ${counter}`)
