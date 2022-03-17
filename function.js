910d695f8af31d6462b1a18a44ae69a943b66b73
eunhyung <ooooo5h91@gmail.com>
2022-03-17 15:54:44 +0900

    function declaration

---
+++ function.js
@@ -0,0 +1,22 @@
+//  Function
+//  - fundamental building block in the program
+//  - subprogram can be used multiple times
+//  - performs a task or calculates a value
+
+// 1. Function declaration
+// function name (param1, param2) {body... return;}
+// one function === one thing
+// naming : doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('은형이가 자바스크립트 공부중');
+log(314);
