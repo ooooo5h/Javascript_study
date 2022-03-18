33ab99b6858073ffa325e6d6bf0f73e1603c67c1
eunhyung <ooooo5h91@gmail.com>
2022-03-18 12:10:23 +0900

    array index position

--- array.js
+++ array.js
@@ -6,3 +6,9 @@
 const arr1 = new Array();
 const arr2 = [1,2];
 
+// 2. Index position
+const fruits = ['단감', '참외'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);  // 제일 마지막 인덱스값 가져오기
