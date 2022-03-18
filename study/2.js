6e39c618c78e0561a7f376f7d3248f9d6d0d8c0f
eunhyung <ooooo5h91@gmail.com>
2022-03-18 09:51:11 +0900

    question answer : man name also null

--- study/2.js
+++ study/2.js
@@ -11,7 +11,7 @@ let boy = {
   man.showName();  
   // 예상했던 결과는 boy는 사라졌어도, boy의 분신을 만들어 man에 넣어두었기때문에 mike가 출력될 줄알았는데.. 
   // Uncaught TypeError: Cannot read properties of null (reading 'name') 라고 뜬다. 왜지??
-
+  // 얕은 복사를 해서 man에 담아두었고, boy가 null이 되었으면, man의 내부도 null이 되기 때문.
 
 
   man.showName();  
