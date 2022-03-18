2a67c2ac757f47a77a71785d1064d2f3800f548b
eunhyung <ooooo5h91@gmail.com>
2022-03-18 22:40:50 +0900

    Q5
    - find() 안에 조건식을 작성하면 그에 대해 일치하는 아이를 반환한다.

--- array-api.js
+++ array-api.js
@@ -53,23 +53,25 @@ console.log(array)    // 원래 array는 변화 없음.
 
 
 class Student {
-constructor(name, age, enrolled, score) {
-    this.name = name;
-    this.age = age;
-    this.enrolled = enrolled;
-    this.score = score;
+    constructor(name, age, enrolled, score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
 }
-}
-const students = [
-new Student('A', 29, true, 45),
-new Student('B', 28, false, 80),
-new Student('C', 30, true, 90),
-new Student('D', 40, false, 66),
-new Student('E', 18, true, 88),
-];
+    const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 66),
+    new Student('E', 18, true, 88),
+    ];
   
   // Q5. find a student with the score 90
   {
+      const result = students.find(student => student.score === 90);
+      console.log(result);
   }
   
   // Q6. make an array of enrolled students
