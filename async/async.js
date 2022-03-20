bf17adc4e8be09585656efe629e91cc866786f30
eunhyung <ooooo5h91@gmail.com>
2022-03-20 21:00:32 +0900

    using async
    - 코드앞에 async를 사용하면, 더 간단하게 promise를 호출할 수 있다

--- async/async.js
+++ async/async.js
@@ -2,11 +2,9 @@
  // clear style of using promise ;)
 
  // 1. async
- function fetchUser(){
-     return new Promise((resolve, reject) => {
-         // do network request in 10secs...
-         resolve('eunhyung');
-     })
+ async function fetchUser(){
+    // do network request in 10secs...
+    return 'eunhyung';
  }
 
  const user = fetchUser();
