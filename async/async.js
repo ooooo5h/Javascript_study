ee9a62e46949d169bc68789908048a9993795143
eunhyung <ooooo5h91@gmail.com>
2022-03-20 21:10:54 +0900

    await
    - 프로미스도 중첩적으로 사용하면 콜백지옥과 같은 문제점이 발생하므로 좋지 않은 코드

--- async/async.js
+++ async/async.js
@@ -10,3 +10,26 @@
  const user = fetchUser();
  user.then(console.log);
  console.log(user);
+
+ // 2. await
+ function delay(ms) {
+     return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+ async function getApple() {
+     await delay(3000);
+     return '사과';
+ }
+
+ async function getBanana() {   // delay가 끝날때까지 기다려. 기다렸다가 바나나를 리턴하는 코드
+     await delay(3000);
+     return '바나나';
+ }
+
+ function pickFruits() {
+     return getApple().then(apple => {
+         return getBanana().then(banana =>  `${apple} + ${banana}`);
+     });
+ }
+
+ pickFruits().then(console.log);
