48d1efa860504cc6b9752a9273ffdb22ff336ab4
eunhyung <ooooo5h91@gmail.com>
2022-03-20 15:31:58 +0900

    many callback bad
    - 콜백함수를 많이 사용하면 코드의 가독성이떨어진다는 단점이 있다.

--- async/callback.js
+++ async/callback.js
@@ -18,3 +18,54 @@ function printWithDelay(print, timeout) {
     setTimeout(print, timeout);
 }
 printWithDelay(()=> console.log('async callback'), 2000);
+
+// Callback Hell example
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(()=> {
+            if (
+                (id === 'eunhyung' && password === 'qwer') ||
+                (id === 'coder' && password === 'qwer')
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(()=> {
+            if (user === 'eunhyung') {
+                onSuccess({name : 'eh', role : 'admin'});
+            } else {
+                onError( new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id, 
+    password, 
+    user => {
+        userStorage.getRoles(
+            user,
+            userWithRole => {
+                alert(
+                    `Hello ${userWithRole.name}, you have a ${userWithRole.role} roll`
+                );
+            },
+            error => {
+                console.log('error');
+            }
+        );
+    }, 
+    error => {
+        console.log(error);
+    }
+);
+// 가독성이 떨어진다는 단점, 디버깅도 상당히 어렵다. 
