66d599b414e9f40220f72303551a3ca3620e1fdd
eunhyung <ooooo5h91@gmail.com>
2022-03-20 15:15:04 +0900

    2 types of callback

--- async/callback.js
+++ async/callback.js
@@ -6,3 +6,15 @@
 console.log('1');
 setTimeout(() => console.log('2'), 1000);  // 1000 = 1초     비동기적인 시스템 방식.
 console.log('3');
+
+// Synchronous callback 동기
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback 비동기 => 나중에 일어남
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+printWithDelay(()=> console.log('async callback'), 2000);
