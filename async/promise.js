0f60c0568684b07a9bdc1c19ffd1984fce59a7f9
eunhyung <ooooo5h91@gmail.com>
2022-03-20 16:06:59 +0900

    then

--- async/promise.js
+++ async/promise.js
@@ -14,3 +14,8 @@ const promise = new Promise((resolve, reject) => {
     }, 2000);
 })
 
+// 2. Consumers : then, catch, finally
+promise.then((value) => {    // promise가 정상적으로 잘 실행이 되어서 마지막에 최종적으로 Resolve라는 함수에 전달이 되고, 전달한 값이 then의 value로 전달이 된다. 
+    console.log(value);
+})
+
