65102c8c4af1c90ff25831b6889a258e0ef33a29
eunhyung <ooooo5h91@gmail.com>
2022-03-20 16:18:10 +0900

    promise chaining
    - by then

--- async/promise.js
+++ async/promise.js
@@ -17,7 +17,7 @@ const promise = new Promise((resolve, reject) => {
 
 // 2. Consumers : then, catch, finally
 promise
-    .then((value) => {    // promise가 정상적으로 잘 실행이 되어서 마지막에 최종적으로 Resolve라는 함수에 전달이 되고, 전달한 값이 then의 value로 전달이 된다. 
+    .then((value) => {    // promise가 정상적으로 잘 실행이  되어서 마지막에 최종적으로 Resolve라는 함수에 전달이 되고, 전달한 값이 then의 value로 전달이 된다. 
     console.log(value);
     })
     .catch(error => {
@@ -26,3 +26,18 @@ promise
     .finally(()=> {console.log('finally')
     }); // 무조건 마지막에 실행되는 아이
 
+
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 1000);
+})
+
+fetchNumber
+.then(num => num * 2)    // then은 값을 전달할 수도 있고 또 다른 비동기인 promise를 전달할 수 있음 2
+.then(num => num * 3)    // 6
+.then(num => {  // 6
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> resolve(num-1), 1000);  // 5
+    });
+})
+.then(num => console.log(num));  //5 
