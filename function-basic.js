5af8ff17cdc72b8ddc984af0aa6d3347097fa161
eunhyung <ooooo5h91@gmail.com>
2022-03-21 16:00:53 +0900

    function with return

--- function-basic.js
+++ function-basic.js
@@ -5,5 +5,13 @@ function doSomething() {
     console.log('hello');
 }
 
+function add(a,b) {
+    const sum = a + b;
+    return sum;
+}
+
 // 함수 호출
 doSomething();
+
+const result = add(1,2);
+console.log(result);
