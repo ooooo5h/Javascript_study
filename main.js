328b71fefe056a72d92453b45ee6f5c00e9c43cf
eunhyung <ooooo5h91@gmail.com>
2022-03-21 17:18:23 +0900

    if don't put number in variable, it's original value is undefined
    - return false

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // false : 0, -0, '', null, undefined
 // true : -1, 't', 'false'
-if (0) {
+
+let num ; // 값을 할당하지 않으면 Undefined가 담김
+if (num) {
     console.log('true!')
 } else {
     console.log('false!')
