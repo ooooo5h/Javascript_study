d2e7632dc03e2d38930e8506d20198f195ef2934
eunhyung <ooooo5h91@gmail.com>
2022-03-21 17:20:00 +0900

    when num is true
    - 뒤의 문장이 실행이 된다

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // false : 0, -0, '', null, undefined
 // true : -1, 't', 'false'
 
-let num ; // 값을 할당하지 않으면 Undefined가 담김
+// let num ; // 값을 할당하지 않으면 Undefined가 담김
+let num = 9;
 if (num) {
     console.log('true!')
 } else {
     console.log('false!')
 }
 
-num && console.log(num)   // num이 false이기 때문에 뒤에 문장 자체가 실행이 안됨
+num && console.log(num)   // num이 true이기 때문에 뒤의 문장이 실행이 됨
