64383023407560d6bf342052d000a953d4749dc6
eunhyung <ooooo5h91@gmail.com>
2022-03-21 17:36:33 +0900

    can't control

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-// false : 0, -0, '', null, undefined
-// true : -1, 't', 'false'
+class Counter {
+    constructor() {
+        this.counter = 0;
+    }
 
-// let num ; // 값을 할당하지 않으면 Undefined가 담김
-let num = 9;
-if (num) {
-    console.log('true!')
-} else {
-    console.log('false!')
+    increase() {
+        this.counter++;
+        console.log(this.counter);
+        if (this.counter % 5 === 0) {
+            console.log('yo')
+        }
+    }
 }
 
-num && console.log(num)   // num이 true이기 때문에 뒤의 문장이 실행이 됨
+const coolCounter = new Counter();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
