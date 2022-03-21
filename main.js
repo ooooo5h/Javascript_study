e33b7173eb475edd81fea41f508561e1e4360047
eunhyung <ooooo5h91@gmail.com>
2022-03-21 17:45:44 +0900

    refactoring code
    - by using call back

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 class Counter {
-    constructor() {
+    constructor(runEveryFiveTimes) {
         this.counter = 0;
+        this.callback = runEveryFiveTimes;
     }
 
-    increase(runIf5Times) {
+    increase() {
         this.counter++;
         console.log(this.counter);
         if (this.counter % 5 === 0) {
-            runIf5Times(this.counter)
+            this.callback(this.counter)
         }
     }
 }
-
-const coolCounter = new Counter();
 
 function alertNum(num) {
     alert(`wow! ${num}`);
 }
 
-function printSomething(num) {
+function printSomething(num) {   // callback은 여기를 가르키고 있음.
     console.log(`wow! ${num}`);
 }
 
-coolCounter.increase(printSomething);
-coolCounter.increase(printSomething);
-coolCounter.increase(printSomething);
-coolCounter.increase(printSomething);
-coolCounter.increase(alertNum);
-coolCounter.increase(printSomething);
+const coolCounter = new Counter(alertNum);   // 여기를 alertNum으로 바꾸면 알림창을 띄움
+
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
+coolCounter.increase();
