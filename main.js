2a62b6d7f0b8bc767d9b64361c43c71a64c1f754
ooooo5h91 <ooooo5h91@gmail.com>
2022-05-08 23:51:07 +0900

    자바스크립트에서의 변수

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function printSomething(num) {   // callback은 여기를 가르키고 있음.
     console.log(`wow! ${num}`);
 }
 
-const coolCounter = new Counter(alertNum);   // 여기를 alertNum으로 바꾸면 알림창을 띄움
+const coolCounter = new Counter();   // 여기를 alertNum으로 바꾸면 알림창을 띄움
 
 coolCounter.increase();
 coolCounter.increase();
