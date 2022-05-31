7393fee76240f159ef1d294053a6ff2a02e7c1d9
ooooo5h91 <ooooo5h91@gmail.com>
2022-05-31 16:55:56 +0900

    dream_coding : 프로미스 개념 적용

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -1,43 +1,38 @@
 // callback hell example
 class UserStorage {
-    loginUser(id, password, onSuccess, onError) {
-        setTimeout(()=> {
-            if (
-                (id === 'ellie' && password === 'dream') ||
-                (id === 'coder' && password === 'academy')
-            ) {
-                onSuccess(id);
-            } else {
-                onError(new Error('not found'));
-            }
-        }, 2000);
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(()=> {
+                if (
+                    (id === 'ellie' && password === 'dream') ||
+                    (id === 'coder' && password === 'academy')
+                ) {
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'));
+                }
+            }, 2000);
+        })
     }
 
-    getRoles(user, onSuccess, onError) {
-        setTimeout(()=>{
-            if (user === 'ellie') {
-                onSuccess({name : 'ellie', role:'admin'});
-            } else {
-                onError(new Error('no access'));
-            }
-        }, 1000);
+    getRoles(user) {
+        return new Promise((resolve, reject)=> {
+            setTimeout(()=>{
+                if (user === 'ellie') {
+                    resolve({name : 'ellie', role:'admin'});
+                } else {
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        })
     }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(
-    id, 
-    password, 
-    (user)=>{
-        userStorage.getRoles(
-            user, 
-            (userWithRole) => {
-                alert(`안녕하세요 ${userWithRole.name} you have a ${userWithRole.role} role`)
-            }, 
-            (error) => {console.log(error)}
-        );
-    }, 
-    (error) => {console.log(error)}
-);
+
+userStorage.loginUser(id, password)
+.then(userStorage.getRoles)
+.then(user => alert(`안녕하세요 ${user.name} you have a ${user.role} role`))
+.catch(console.log);
